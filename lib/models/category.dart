c24a1daa56911f7d620f87e93ae1d55e4f6ed2c1
Eduardo Vega <eduardovr1999@gmail.com>
2022-07-27 13:53:12 +0200

    Create Category model

---
+++ lib/models/category.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class Category {
+
+  final int id;
+  final String name;
+  final IconData icon;
+  final Color iconColor;
+  final Color backgroundColor;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.icon,
+    required this.iconColor,
+    required this.backgroundColor
+  });
+  
+} 
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,2 @@
+export 'package:my_piggy_bank_app/models/category.dart';
 export 'package:my_piggy_bank_app/models/transaction.dart';
