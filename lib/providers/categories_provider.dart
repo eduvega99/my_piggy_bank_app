04568f3c1b89230b1a12724d17a40f4522edab9f
Eduardo Vega <eduardovr1999@gmail.com>
2022-08-04 22:07:57 +0200

    Refactor Categories provider

--- lib/providers/categories_provider.dart
+++ lib/providers/categories_provider.dart
@@ -4,27 +4,29 @@ import 'package:my_piggy_bank_app/models/category.dart';
 class CategoriesProvider {
   
   static final Map<int, Category> _categories = {};
+  static bool _isCategoriesLoaded = false;
 
   Future<Category?> getCategory(int id) async {
-    if (_categories.isEmpty) {
-      _loadCategories();
+    if (!_isCategoriesLoaded) {
+      await _loadCategories();
     }
     return _categories[id];
   }
 
   Future<List<Category>> getCategories() async {
-    if (_categories.isEmpty) {
-      _loadCategories();
+    if (!_isCategoriesLoaded) {
+      await _loadCategories();
     }
     return List.generate(_categories.length, (index) => _categories[index]!);
   }
 
-  void _loadCategories() async {
+  Future<void> _loadCategories() async {
     final List<dynamic> data = await getJsonData('categories');
     for (final json in data) {
       final category = Category.fromJson(json);
       _categories[category.id] = category;
     }
+    _isCategoriesLoaded = true;
   }
 
 }
