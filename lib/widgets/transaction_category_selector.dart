5659595ad89297df69870809e728d3944a2ba647
Eduardo Vega <eduardovr1999@gmail.com>
2022-08-04 22:15:52 +0200

    Create category selector with dynamic json categories

--- lib/widgets/transaction_category_selector.dart
+++ lib/widgets/transaction_category_selector.dart
@@ -1,85 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:my_piggy_bank_app/models/category.dart';
+import 'package:my_piggy_bank_app/providers/categories_provider.dart';
+
 
 class TransactionCategorySelector extends StatelessWidget {
   
-  final List<Map<String, dynamic>> _categories = [
-    {
-      'name': 'Salud e higiene',
-      'icon': Icons.local_hospital,
-      'iconColor': Colors.green,
-      'backgroundColor': Colors.green[100]!,
-      'isSelected': false,
-    },
-    {
-      'name': 'Alimentación',
-      'icon': Icons.restaurant,
-      'iconColor': Colors.orange,
-      'backgroundColor': Colors.orange[100]!,
-      'isSelected': false,
-    },
-    {
-      'name': 'Transporte',
-      'icon': Icons.directions_car,
-      'iconColor': Colors.purple,
-      'backgroundColor': Colors.purple[100]!,
-      'isSelected': false,
-    },
-    {
-      'name': 'Supermercado',
-      'icon': Icons.shopping_cart,
-      'iconColor': Colors.indigo,
-      'backgroundColor': Colors.indigo[100]!,
-      'isSelected': true,
-    },
-    {
-      'name': 'Cuentas y pagos',
-      'icon': Icons.receipt,
-      'iconColor': Colors.blue,
-      'backgroundColor': Colors.blue[100]!,
-      'isSelected': false,
-    },
-    {
-      'name': 'Compras',
-      'icon': Icons.shopping_bag,
-      'iconColor': Colors.brown,
-      'backgroundColor': Colors.brown[100]!,
-      'isSelected': false,
-    },
-    {
-      'name': 'Ocio',
-      'icon': Icons.interests_rounded,
-      'iconColor': Colors.pink,
-      'backgroundColor': Colors.pink[100]!,
-      'isSelected': false,
-    },
-    {
-      'name': 'Otros',
-      'icon': Icons.more_horiz,
-      'iconColor': Colors.teal,
-      'backgroundColor': Colors.teal[100]!,
-      'isSelected': false,
-    },
-  ];
+  List<Category> _categories = [];
 
   TransactionCategorySelector({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) { 
-    final halfSizeArray = (_categories.length / 2).ceil();
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Column(
-        children: [
-          _getRow(_categories.sublist(0, halfSizeArray)),
-          const SizedBox(height: 5),
-          _getRow(_categories.sublist(halfSizeArray, _categories.length)),
-        ],
-      ),
+    final provider = CategoriesProvider();
+    return FutureBuilder(
+      future: provider.getCategories(),
+      builder: ( _ , AsyncSnapshot<List<Category>> snapshot) {
+        _categories = snapshot.hasData ? snapshot.data! : [];
+        final halfSizeArray = (_categories.length / 2).ceil();
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Column(
+            children: [
+              _getRow(_categories.sublist(0, halfSizeArray)),
+              const SizedBox(height: 5),
+              _getRow(_categories.sublist(halfSizeArray, _categories.length)),
+            ],
+          ),
+        );
+      },
     );
   }
 
-  Row _getRow(List<Map<String, dynamic>> categories) {
+  Row _getRow(List<Category> categories) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: List.generate(
@@ -87,11 +40,8 @@ class TransactionCategorySelector extends StatelessWidget {
         (index) {
           final category = categories[index];
           return _Button(
-            tooltip: category['name'],
-            icon: category['icon'],
-            iconColor: category['iconColor'],
-            backgroundColor: category['backgroundColor'],
-            isSelected: category['isSelected']
+            category: category, 
+            isSelected: false
           );
         }
       )
@@ -102,19 +52,13 @@ class TransactionCategorySelector extends StatelessWidget {
 
 class _Button extends StatelessWidget {
   
-  final IconData icon;
-  final Color iconColor;
-  final Color backgroundColor;
+  final Category category;
   final bool isSelected;
-  final String? tooltip;
   
   const _Button({
     Key? key, 
-    required this.icon, 
-    required this.iconColor, 
-    required this.backgroundColor,
-    this.isSelected = false, 
-    this.tooltip 
+    required this.category, 
+    this.isSelected = false,
   }) : super(key: key);
 
   @override
@@ -125,12 +69,12 @@ class _Button extends StatelessWidget {
       alignment: Alignment.center,
       child: CircleAvatar(
         radius: 18,
-        backgroundColor: backgroundColor,
+        backgroundColor: category.backgroundColor,
         child: IconButton(
-          icon: Icon(icon, size: 20),
-          color: iconColor,
+          icon: Icon(category.icon, size: 20),
+          color: category.iconColor,
           splashRadius: 20,
-          tooltip: tooltip,
+          tooltip: category.name,
           onPressed: () {
             print('Hola mundo');
           },
